97bb2450046915234a829673962d8d392f154518
Engincan Danabaş <60680749+engincandanabas@users.noreply.github.com>
2024-11-17 01:48:46 +0300

    Update
    New input system
    Gamepad controls
    Charachter animation
    Jump

--- Assets/Scripts/Managers/GravityController.cs
+++ Assets/Scripts/Managers/GravityController.cs
@@ -5,31 +5,25 @@ public class GravityController : MonoBehaviour
     public delegate void GravityChanged(bool flippedNormal);
     public static event GravityChanged OnGravityChange;
 
+    GravitySnap controls;
 
     private bool isGravityFlipped = false;
     private bool canChangeGravity = false;
-    void Update()
+
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.G)) // Example key to flip gravity
-        {
-            if(canChangeGravity)
-            {
-                isGravityFlipped = !isGravityFlipped;
-
-                // Flip gravity (example)
-                Physics2D.gravity = isGravityFlipped ? new Vector2(0, 9.8f) : new Vector2(0, -9.8f);
-
-                OnGravityChange?.Invoke(isGravityFlipped);
-            }
-        }
+        controls=new GravitySnap();
+        controls.Player.Gravity.performed += ctx => ChangeGravity();
     }
     private void OnEnable()
     {
+        controls.Player.Enable();
         PlayerTrigger.OnGroundEnter += ChangeGroundState;
         PlayerTrigger.OnGroundExit += ChangeGroundState;
     }
     private void OnDisable()
     {
+        controls.Player.Disable();
         PlayerTrigger.OnGroundEnter -= ChangeGroundState;
         PlayerTrigger.OnGroundExit -= ChangeGroundState;
     }
@@ -37,4 +31,17 @@ public class GravityController : MonoBehaviour
     {
         canChangeGravity = isGround;
     }
+    private void ChangeGravity()
+    {
+        if (canChangeGravity)
+        {
+            isGravityFlipped = !isGravityFlipped;
+
+            // Flip gravity (example)
+            Physics2D.gravity = isGravityFlipped ? new Vector2(0, 9.8f) : new Vector2(0, -9.8f);
+
+            OnGravityChange?.Invoke(isGravityFlipped);
+        }
+    }
+   
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float airMovementBoundLength = 1;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private int maxJumps = 2;
+    private int jumpCount = 0;
 
     private Animator animator;
     private Rigidbody2D rb;
@@ -13,8 +17,10 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 velocity;
     private Vector2 playerPos;
     private bool isGrounded;
+    private bool isJump = false;
+    float moveInput;
 
-    private void Start()
+    private void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
@@ -24,17 +30,19 @@ public class PlayerMovement : MonoBehaviour
     {
         PlayerTrigger.OnGroundExit += ChangeGroundState;
         PlayerTrigger.OnGroundEnter += ChangeGroundState;
+        PlayerTrigger.OnGroundEnter += ResetJumpCount;
+        GravityController.OnGravityChange += GravityChanged;
     }
     private void OnDisable()
     {
         PlayerTrigger.OnGroundExit -= ChangeGroundState;
         PlayerTrigger.OnGroundEnter -= ChangeGroundState;
+        PlayerTrigger.OnGroundEnter -= ResetJumpCount;
+        GravityController.OnGravityChange -= GravityChanged;
+
     }
     private void FixedUpdate()
     {
-        // Horizontal movement
-        float moveInput = Input.GetAxis("Horizontal");
-
         // trigger anim
         if (moveInput != 0)
             animator.SetBool("isRunning", true);
@@ -48,13 +56,14 @@ public class PlayerMovement : MonoBehaviour
         // flip rotation
         if (moveInput > 0)
             spriteRenderer.flipX = false;
-        else if(moveInput < 0)
+        else if (moveInput < 0)
             spriteRenderer.flipX = true;
 
-        if(isGrounded)
+
+        if (isGrounded || isJump)
         {
             transform.Translate(new Vector3(moveInput * moveSpeed * Time.deltaTime, 0, 0));
-            if(!dustParticle.isPlaying)
+            if (!dustParticle.isPlaying && !isJump)
                 dustParticle.Play();
         }
         else
@@ -62,10 +71,63 @@ public class PlayerMovement : MonoBehaviour
             transform.Translate(new Vector3(moveInput * moveSpeed * Time.deltaTime, 0, 0));
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, playerPos.x - airMovementBoundLength, playerPos.x + airMovementBoundLength), transform.position.y, 0);
         }
+
+
     }
     private void ChangeGroundState(bool isGround)
     {
+        Debug.Log("Is Grounded : " + isGround);
         isGrounded = isGround;
-        playerPos= transform.position;
+        if (!isGround)
+            playerPos = transform.position;
+        else
+        {
+            if (animator.GetBool("isJumping"))
+            {
+                animator.SetBool("isJumping", false);
+            }
+            if (animator.GetBool("isFalling"))
+            {
+                animator.SetBool("isFalling", false);
+            }
+        }
     }
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            animator.SetBool("isJumping", true);
+            isJump = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); // Apply jump force
+            isGrounded = false; // Temporarily set to false until collision checks
+            dustParticle.Stop();
+            jumpCount++;
+        }
+
+    }
+    private void ResetJumpCount(bool isGround)
+    {
+        if (isGround)
+        {
+            isJump = false;
+            jumpCount = 0;
+        }
+    }
+    private void GravityChanged(bool isFlipped)
+    {
+        jumpForce *= -1;
+        animator.SetBool("isFalling", true);
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>().x;
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Jump();
+        }
+    }
+
 }
